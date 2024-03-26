f16fd30ab6d5c61f1b61f6953a8c78b71404cc84
Tan Shaoting <2399330341@qq.com>
2024-03-26 19:27:18 +0800

    1927

--- front/src/main/java/com/management/front/HelloApplication.java
+++ front/src/main/java/com/management/front/HelloApplication.java
@@ -1,10 +1,13 @@
 package com.management.front;
 
 import com.management.front.controller.HomePage;
+import com.management.front.controller.Menu;
 import com.management.front.controller.WeekTimeTable;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,8 +19,7 @@ import static com.management.front.util.HttpClientUtil.sendAndReceiveObject;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        
-        Scene scene = new Scene(new HomePage());
+        Scene scene = new Scene(new Menu(), 800, 600);
         stage.setScene(scene);
         stage.show();
     }
--- front/src/main/java/com/management/front/controller/Menu.java
+++ front/src/main/java/com/management/front/controller/Menu.java
@@ -2,8 +2,11 @@ package com.management.front.controller;
 
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
-public class Menu extends TreeView<String> {
+public class Menu extends HBox {
+    TreeView<String> menu = new TreeView<>();
     public Menu() {
         TreeItem<String> root = new TreeItem<>("菜单");
         root.setExpanded(true);
@@ -12,6 +15,20 @@ public class Menu extends TreeView<String> {
         TreeItem<String> item3 = new TreeItem<>("教师管理");
         TreeItem<String> item4 = new TreeItem<>("班级管理");
         root.getChildren().addAll(item1, item2, item3, item4);
-        this.setRoot(root);
+        menu.setRoot(root);
+        this.getChildren().add(menu);
+        this.getChildren().add(new Pane());
+        menu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            switch (newValue.getValue()) {
+                case "学生管理": this.getChildren().set(1,new WeekTimeTable());
+                    break;
+                case "课程管理": this.getChildren().set(1,new HomePage());
+                    break;
+                case "教师管理":
+                    break;
+                case "班级管理":
+                    break;
+            }
+        });
     }
 }
