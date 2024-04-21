853e146edd258f5c6eb006d2a764e7eb483fc692
RunAbin <1960567465@qq.com>
2024-04-21 21:53:23 +0800

    666

--- server/src/main/java/com/management/server/controllers/HonorController.java
+++ server/src/main/java/com/management/server/controllers/HonorController.java
@@ -2,12 +2,15 @@ package com.management.server.controllers;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import com.management.server.models.Event;
 import com.management.server.models.Honor;
 import com.management.server.models.Person;
 import com.management.server.models.Student;
 import com.management.server.payload.response.DataResponse;
+import com.management.server.repositories.EventRepository;
 import com.management.server.repositories.HonorRepository;
 import com.management.server.repositories.StudentRepository;
+import com.management.server.util.CommonMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,49 +20,59 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
+@RestController
 public class HonorController {
     @Autowired
     HonorRepository honorRepository;
     @Autowired
     StudentRepository studentRepository;
+
+    @Autowired
+    EventRepository eventRepository;
     @PostMapping("/getAllHonors")
     public DataResponse getAllHonors()
     {
+        Honor h=new Honor();
+        h.setName("good");
+        honorRepository.save(h);
+        System.out.println(honorRepository.findAll());
         return new DataResponse(200,honorRepository.findAll(),null);
     }
     @PostMapping("/getHonorsByStudent")
     public DataResponse getHonorByStudent(@RequestBody Map m)
     {
-
-        Student s=studentRepository.findByStudentId((String) m.get("studentId"));
+        String username = CommonMethod.getUsername();
+        Student s=studentRepository.findByStudentId(username);
         List<Honor> list=honorRepository.findByPerson(s);
         return new DataResponse(200,list,null);
     }
     @PostMapping("/addHonor")
     public DataResponse addhonor(@RequestBody Map m){
-        Student s=studentRepository.findByStudentId((String) m.get("studentId"));
-        Set<Person> studentList=new HashSet<>();
-        studentList.add(s);
+        String username = CommonMethod.getUsername();
+        Student s=studentRepository.findByStudentId(username);
+        Event e =eventRepository.findEventByEventId("1");//
+
         Honor honor=new Honor();
         honor.setName((String) m.get("name"));
         honor.setTime((String) m.get("time"));
         honor.setMessage((String) m.get("message"));
         honor.setDepartment((String) m.get("department"));
+        honor.setPerson(s);
+        honor.setEvent(e);
         honorRepository.save(honor);
         return new DataResponse(0,null,"添加成功");
     }
     @PostMapping("/deleteHonor")
     public DataResponse deleteHonor(@RequestBody Map m)
     {
-        honorRepository.deleteByHonorId((String)m.get("eventId"));
+        honorRepository.deleteByHonorId((String)m.get("honorId"));
         return new DataResponse(0,null,"删除成功");
     }
     @PostMapping("/updateHonor")
     public DataResponse updateHonor(@RequestBody Map m)
     {
         //save()方法既可以用于保存新的实体，也可以用于更新已存在的实体。
-        Honor honor=honorRepository.findByHonorId((String) m.get("eventId"));
+        Honor honor=honorRepository.findByHonorId((String) m.get("honorId"));
         List<Map> personsMap = (List<Map>) m.get("persons");
         System.out.println(personsMap);
         Set<Person> persons = new HashSet<>();
