bea391115ac479a9af02eedfa5fc06aba1e8f7c5
wzksduer <144761429+wzksduer@users.noreply.github.com>
2024-05-04 17:17:35 +0800

    5.4

--- client/src/main/java/com/management/client/ClientApplication.java
+++ client/src/main/java/com/management/client/ClientApplication.java
@@ -4,6 +4,9 @@ package com.management.client;
 import com.management.client.page.LoginPage;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.burningwave.core.classes.Modules;
 
@@ -12,12 +15,10 @@ import static com.management.client.util.HttpClientUtil.sendAndReceiveDataRespon
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) {
-
         sendAndReceiveDataResponse("/test/addTestData",null);
         Scene scene = new Scene(new LoginPage(), 300, 400);
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
